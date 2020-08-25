CTSRD-CHERI/TagController

commit 8c05a3d331a3eda319ffff13a46fef64a8c567b4

128 -> 64 bit bus width change (pdr32)

TagController/MultiLevelTagLookup.bsv
@@ -304,7 +304,7 @@ module mkMultiLevelTagLookup #(
       z = True; // we need zeroing
       Integer i = 0;
       CheriPhyByteOffset nodeOffset = byteOffset&(~0 << (t.groupFactorLog - 3));
-      for (i = 0; i < (div(t.groupFactor,8)); i = i + 1) begin
+      for (i = 0; i < (div(t.groupFactor,16)); i = i + 1) begin
         wbyteE[nodeOffset+fromInteger(i)] = True;
       end
     end
@@ -663,7 +663,7 @@ module mkMultiLevelTagLookup #(
             Integer i = 0;
             Integer bot = 0;
             for (i = 0; i < valueOf(CapsPerFlit); i = i + 1) begin
-              Bit#(CapBytes) capBEs = pack(wop.byteEnable)[bot+valueOf(CapBytes)-1:bot];
+              Bit#(CapBytes) capBEs = pack(wop.byteEnable)[bot+valueOf(TMin#(CheriBusBytes, CapBytes))-1:bot];
               capEnable[i] = (capBEs == 0) ? False:True;
               bot = bot + valueOf(CapBytes);
             end

TagController/AXI_Helpers.bsv
@@ -56,11 +56,11 @@ instance DefaultValue#(RspFlit#(id_, data_, tag_));
   function defaultValue = tagged Write defaultValue;
 endinstance
 
-typedef ReqFlit#(id_, addr_, 128, CapsPerFlit) MemReq#(numeric type id_, numeric type addr_);
-typedef RspFlit#(id_, 128, CapsPerFlit)        MemRsp#(numeric type id_);
+typedef ReqFlit#(id_, addr_, 64, CapsPerFlit) MemReq#(numeric type id_, numeric type addr_);
+typedef RspFlit#(id_, 64, CapsPerFlit)        MemRsp#(numeric type id_);
 
-typedef ReqFlit#(id_, addr_, 128, 0) DRAMReq#(numeric type id_, numeric type addr_);
-typedef RspFlit#(id_, 128, 0)        DRAMRsp#(numeric type id_);
+typedef ReqFlit#(id_, addr_, 64, 0) DRAMReq#(numeric type id_, numeric type addr_);
+typedef RspFlit#(id_, 64, 0)        DRAMRsp#(numeric type id_);
 
 // Request ranslators between AXI and CHERI Memory
 
@@ -121,13 +121,13 @@ function DRAMReq#(id_, addr_) mem2axi_req(CheriMemRequest mr)
     tagged Write .w: begin
       //XXX horrible hack - from 40 bits restriction
       // support addresses up to 64 bits, only considers bottom 40 bits
-      Bit#(64) tmp = zeroExtend(pack(mr.addr) & (~0 << pack(BYTE_16)));
+      Bit#(64) tmp = zeroExtend(pack(mr.addr) & (~0 << pack(BYTE_8)));
       req = tagged Write WriteReqFlit{
         aw: AXI4_AWFlit{
           awid: truncate(pack(getReqId(mr))),
           awaddr: truncate(tmp),
-          awlen: 0,
-          awsize: unpack(pack(BYTE_16)), // 16 bytes
+          awlen: w.length,
+          awsize: unpack(pack(BYTE_8)), // 8 bytes
           awburst: INCR,
           awlock: NORMAL,
           awcache: ((w.uncached) ? 0:15), // unached or fully cached

TagController/CacheCore/CacheCore.bsv
@@ -1438,8 +1438,8 @@ module mkCacheCore#(Bit#(16) cacheId,
                 Integer i;
                 for (i=0; i<valueOf(CapsPerFlit); i=i+1) begin
                   Integer bot = i*valueOf(CapBytes);
-                  Integer top = bot + valueOf(CapBytes) - 1;
-                  Bit#(CapBytes) capBytes = pack(wop.byteEnable)[top:bot];
+                  Integer top = bot + valueOf(CapBytes);
+                  Bit#(CapBytes) capBytes = pack(wop.byteEnable)[min(top,valueOf(CheriBusBytes))-1:bot];
                   if (capBytes != 0) capTags[i] = wop.data.cap[i];
                 end
                 //$display("capTags: %x", capTags);

TagController/MemTypesCHERI.bsv
@@ -128,7 +128,7 @@ endinstance
 `endif
 typedef 40 AddrWidth;
 `ifdef USECAP
-  typedef TDiv#(CheriBusBytes,CapBytes) CapsPerFlit;
+  typedef TMax#(TDiv#(CheriBusBytes,CapBytes),1) CapsPerFlit;
   typedef Vector#(CapsPerFlit,Bool) CapTags;
   typedef Bit#(TSub#(AddrWidth,TLog#(CapBytes))) CapNumber;
   typedef struct {

TagController/TagControllerAXI.bsv
@@ -59,19 +59,19 @@ interface TagControllerAXI#(
   method Action clear;
 endinterface
 
-module mkTagControllerAXI(TagControllerAXI#(id_, addr_,128))
+module mkTagControllerAXI(TagControllerAXI#(id_, addr_,64))
   provisos (Add#(a__, id_, SizeOf#(ReqId)), Add#(b__, TAdd#(id_, 1), SizeOf#(ReqId)), Add#(c__, addr_, 64));
   let tmp <- mkDbgTagControllerAXI(Invalid);
   return tmp;
 endmodule
-module mkDbgTagControllerAXI#(Maybe#(String) dbg)(TagControllerAXI#(id_, addr_,128))
+module mkDbgTagControllerAXI#(Maybe#(String) dbg)(TagControllerAXI#(id_, addr_,64))
   provisos (Add#(a__, id_, SizeOf#(ReqId)), Add#(b__, TAdd#(id_, 1), SizeOf#(ReqId)), Add#(c__, addr_, 64));
   let    clk <- exposeCurrentClock;
   let newRst <- mkReset(0, True, clk);
   TagControllerIfc tagCon <- mkTagController(reset_by newRst.new_rst);
   //Workaround: these are being enqueued while full in Piccolo. Made the buffer size larger (32 from 4)
-  AXI4_Shim#(id_, addr_, 128, 0, CapsPerFlit, 0, 0, CapsPerFlit) shimSlave  <- mkAXI4ShimUGSizedFIFOF32;
-  AXI4_Shim#(TAdd#(id_,1), addr_, 128, 0, 0, 0, 0, 0) shimMaster <- mkAXI4ShimUGSizedFIFOF32;
+  AXI4_Shim#(id_, addr_, 64, 0, CapsPerFlit, 0, 0, CapsPerFlit) shimSlave  <- mkAXI4ShimUGSizedFIFOF32;
+  AXI4_Shim#(TAdd#(id_,1), addr_, 64, 0, 0, 0, 0, 0) shimMaster <- mkAXI4ShimUGSizedFIFOF32;
   FIFO#(Bit#(0)) limiter <- mkFIFO1;
 
   // Rules to feed the tag controller from the slave AXI interface

commit b02ed2d30985227ca6c25d1e1224ba4ebc18dc09

MultiLevelTagLookup: Fix byte-enables for zeroing tag writes
This regressed in 8c05a3d3, the first commit for supporting capabilities
split across multiple flits. Revert it back to turning bits into bytes,
as it is in cheri-cpu. Otherwise we fail to zero out half of a line (or
one byte less than half a line if byteOffset is for the upper half).

TagController/MultiLevelTagLookup.bsv
@@ -315,7 +315,7 @@ module mkMultiLevelTagLookup #(
       z = True; // we need zeroing
       Integer i = 0;
       CheriPhyByteOffset nodeOffset = byteOffset&(~0 << (t.groupFactorLog - 3));
-      for (i = 0; i < (div(t.groupFactor,16)); i = i + 1) begin
+      for (i = 0; i < (div(t.groupFactor,8)); i = i + 1) begin
         wbyteE[nodeOffset+fromInteger(i)] = True;
       end
     end